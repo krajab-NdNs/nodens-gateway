5e5ef8e44c5e3f172d60a4d63554db725d7f7abd
krajab-NdNs <khalid@nodens.eu>
2024-05-21 15:13:51 +0100

    small checks

--- src/nodens/gateway/nodens_fns.py
+++ src/nodens/gateway/nodens_fns.py
@@ -1093,6 +1093,7 @@ class OccupantHist:
         for idx, sensor in enumerate(self.sensor_id):    # For each sensor
             self.outputs.append(self.Outputs)
             self.outputs[idx].sensor_id = sensor
+            nodens.logger.info(f"{idx}: {self.outputs[idx].sensor_id}")
             if len(self.id[idx]) > 0:
                 # Determine track to send
                 if max(self.tot_dist[idx]) >= thresh_distance: # Distance threshold at 0 for now, until UD sig tid is found.
@@ -1126,6 +1127,8 @@ class OccupantHist:
             # Room heatmaps
             self.room_heatmap[idx].prepare_heatmap_string()
             self.outputs[idx].heatmap_string = self.room_heatmap[idx].heatmap_string
+
+            nodens.logger.info(f"track_id: {self.outputs[idx].track_id}")
 
 
     
