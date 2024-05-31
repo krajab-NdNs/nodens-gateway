db54fa48ee38e02ccbcd1cc9ebbaf557499f089c
krajab-NdNs <khalid@nodens.eu>
2024-05-31 12:03:47 +0100

    bug

--- src/nodens/gateway/nodens_serv.py
+++ src/nodens/gateway/nodens_serv.py
@@ -128,7 +128,6 @@ def on_message_sensorN(client, userdata, msg):
                 data = base64.b64decode(mqttData['data'])
             except:
                 data = mqttData['data']
-            nodens.logger.info(f"DATA: {data}")
             str_data = str(data[0])
             data_int = [data[0]]
 
@@ -292,7 +291,7 @@ def on_message_sensorN(client, userdata, msg):
             elif (mqttData['type'] == 'json'):
                 nodens.logger.info("JSON type: {}".format(mqttData))
             # Otherwise process occupancy info
-            elif data['type'] != 'heartbeat':
+            elif json.loads(data)["type"] != 'heartbeat':
                 ndns_fns.counts.update(mqttData['addr'], 'basic')
                 ndns_fns.sm.update(mqttData)
                 mqttOcc = json.loads(data)
