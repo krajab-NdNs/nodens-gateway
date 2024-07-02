e6dde273e89487bfe9dba9f4a949b961d42b471b
krajab-NdNs <khalid@nodens.eu>
2024-07-02 19:58:45 +0100

    test

--- src/nodens/gateway/nodens_serv.py
+++ src/nodens/gateway/nodens_serv.py
@@ -634,12 +634,12 @@ def on_message_sensorN(client, userdata, msg):
                 mqttData_SAVE = []
                 mqttData_SAVEFull = []
 
-                # Check config if necessary
-                sens_idx = ndns_fns.sm.sensor_id.index(mqttDataFinal['addr'])
-                if (T - ndns_fns.sm.last_config_check_time[sens_idx]).seconds > 15*60:
-                    ndns_fns.message_pipeline.config_check(mqttDataFinal['addr'])
-                else:
-                    nodens.logger.warning(f"TIME: {(T - ndns_fns.sm.last_config_check_time[sens_idx]).seconds}")
+        # Check config if necessary
+        sens_idx = ndns_fns.sm.sensor_id.index(mqttDataFinal['addr'])
+        if (T - ndns_fns.sm.last_config_check_time[sens_idx]).seconds > 15*60:
+            ndns_fns.message_pipeline.config_check(mqttDataFinal['addr'])
+        else:
+            nodens.logger.warning(f"TIME: {(T - ndns_fns.sm.last_config_check_time[sens_idx]).seconds}")
 
         # except:
         #     pass
