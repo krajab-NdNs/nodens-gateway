7b3ae708211c37ad024544586514dc3f1d521ff0
krajab-NdNs <khalid@nodens.eu>
2024-07-03 12:00:51 +0100

    Bug fixes for config checks

--- src/nodens/gateway/nodens_serv.py
+++ src/nodens/gateway/nodens_serv.py
@@ -639,8 +639,10 @@ def on_message_sensorN(client, userdata, msg):
             sens_idx = ndns_fns.sm.sensor_id.index(mqttData['addr'])
             if ndns_fns.sm.last_config_check_time[sens_idx] != []:
                 if (T - ndns_fns.sm.last_config_check_time[sens_idx]).seconds > 15*60:
-                    ndns_fns.message_pipeline.config_check(mqttData['addr'])
-                    ndns_fns.sm.last_config_check_time[sens_idx] = T
+                    sen_idx_s = ndns_fns.si.connected_sensors.index(mqttData['addr'])
+                    if (ndns_fns.si.num_occ[sen_idx_s] == 0):
+                        ndns_fns.message_pipeline.config_check(mqttData['addr'])
+                        ndns_fns.sm.last_config_check_time[sens_idx] = T
 
         # except:
         #     pass
